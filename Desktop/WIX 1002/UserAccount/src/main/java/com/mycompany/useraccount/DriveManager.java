7bad54f5a288791fc57e622e08829726d5f94a5b
Ooiky0625 <kwongyang78@gmail.com>
2023-12-19 18:13:56 +0800

    first commit

---
+++ Desktop/WIX 1002/UserAccount/src/main/java/com/mycompany/useraccount/DriveManager.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.useraccount;
+
+import java.sql.Connection;
+
+/**
+ *
+ * @author kwong
+ */
+class DriveManager {
+
+    static Connection getConnection(String jdbcmysqllocalhost3306user_accountzeroDat) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
---
+++ Desktop/WIX 1002/UserAccount/src/main/java/com/mycompany/useraccount/UserAccount.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.useraccount;
+
+/**
+ *
+ * @author kwong
+ */
+public class UserAccount {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
